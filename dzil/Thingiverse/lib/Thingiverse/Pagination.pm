f6475b26fd86e0c7590ed3ca1413411d9e6a0937
Steven Parker <sparker@manta.com>
2015-02-01 22:21:34 -0500

    added remaining stubs from dzil to be added to the converted

---
+++ dzil/Thingiverse/lib/Thingiverse/Pagination.pm
@@ -0,0 +1,33 @@
+package Thingiverse::Pagination;
+use strict;
+use warnings;
+use Moose;
+use Moose::Util::TypeConstraints;
+use Data::Dumper;
+use Carp;
+
+# ABSTRACT: a really awesome library
+
+=head1 SYNOPSIS
+
+  ...
+
+=method method_x
+
+This method does something experimental.
+
+=method method_y
+
+This method returns a reason.
+
+=head1 SEE ALSO
+
+=for :list
+* L<Your::Module>
+* L<Your::Package>
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
