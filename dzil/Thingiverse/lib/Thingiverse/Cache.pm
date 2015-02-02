a1de64ae47b70edb3ab40a187a454a1d697e1dd2
Steven Parker <sparker@manta.com>
2015-02-02 13:21:26 -0500

    moved tests I've written to the dzil test directory

--- dzil/Thingiverse/lib/Thingiverse/Cache.pm
+++ dzil/Thingiverse/lib/Thingiverse/Cache.pm
@@ -23,8 +23,45 @@ This method returns a reason.
 =head1 SEE ALSO
 
 =for :list
-* L<Your::Module>
-* L<Your::Package>
+* L<Thingiverse>
+* L<Thingiverse::User>
+* L<Thingiverse::User::List>
+* L<Thingiverse::Cache>
+* L<Thingiverse::Thing>
+* L<Thingiverse::Thing::List>
+* L<Thingiverse::Tag>
+* L<Thingiverse::Tag::List>
+* L<Thingiverse::Collection>
+* L<Thingiverse::Collection::List>
+* L<Thingiverse::Category>
+* L<Thingiverse::File>
+* L<Thingiverse::File::List>
+* L<Thingiverse::Image>
+* L<Thingiverse::SizedImage>
+* L<Thingiverse::Copy>
+* L<Thingiverse::Pagination>
+* L<Thingiverse::Group>
+=cut
+
+extends('Thingiverse');
+
+our $object_ids = {
+  User    => 'name',
+  Thing   => 'id',
+};
+
+has _object_cache   => ( isa => 'HashRef', is => 'rw', required => 0, );
+
+sub find_me_in_cache {
+  my $self = shift;
+  print "$self\n";
+}
+
+sub put_me_in_cache {
+  my $self = shift;
+  print "$self\n";
+}
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
