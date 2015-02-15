9757d67d09dc23ef6f2638c165bbb074aa4c467b
paul <paulsalcido.79@gmail.com>
2015-02-14 21:15:35 -0800

    Copy compiles, but has no tests.

--- lib/Thingiverse/Copy.pm
+++ lib/Thingiverse/Copy.pm
@@ -1,19 +1,11 @@
 package Thingiverse::Copy;
-use strict;
-use warnings;
 use Moose;
-use Moose::Util::TypeConstraints;
-use Data::Dumper;
-use Carp;
 
-use JSON;
 use Thingiverse::Types;
 use Thingiverse::Image;
 use Thingiverse::User;
 
-extends('Thingiverse');
-
-our $api_base = "/copies/";
+extends('Thingiverse::Object');
 
 # ABSTRACT: a really awesome library
 
@@ -45,43 +37,23 @@ our $api_base = "/copies/";
 * L<Thingiverse::Group>
 =cut
 
-has id             => ( isa => 'ID',                      is => 'ro', required => 1, );
-has _original_json => ( isa => 'Str',                     is => 'ro', required => 0, );
-has url            => ( isa => 'Str',                     is => 'ro', required => 0, );
-has public_url     => ( isa => 'Str',                     is => 'ro', required => 0, );
-has added          => ( isa => 'Str',                     is => 'ro', required => 0, );
-has like_count     => ( isa => 'ThingiCount',             is => 'ro', required => 0, );
-has description    => ( isa => 'Str',                     is => 'ro', required => 0, );
-has is_liked       => ( isa => 'Any',                     is => 'ro', required => 0, );
-has maker          => ( isa => 'Thingiverse::User',       is => 'ro', required => 0, );
-has thumbnail      => ( isa => 'Str',                     is => 'ro', required => 0, );
-has images_url     => ( isa => 'Str',                     is => 'ro', required => 0, );
-
-around BUILDARGS => sub {
-  my $orig = shift;
-  my $class = shift;
-  my $id;
-  my $json;
-  my $hash;
-  # first we check if we can by-pass making an API call to Thingiverse, since the hash was populated via a seperate call
-  if ( @_ == 1 && ref $_[0] eq 'HASH' && ${$_[0]}{'just_bless'} && ${$_[0]}{'id'}) {
-#   print "just bless this thing!\n";
-    delete ${$_[0]}{'just_bless'};
-    return $class->$orig(@_);
-  } elsif ( @_ == 1 && !ref $_[0] ) {
-    $id = $_[0];
-  } elsif ( @_ == 1 && ref $_[0] eq 'HASH' && ${$_[0]}{'id'} ) { # passed a hashref to a hash containing key 'id'
-    $id = ${$_[0]}{'id'};
-  } elsif ( @_ == 2 && $_[0] eq 'id' ) { # passed a hashref to a hash containing key 'id'
-    $id = $_[1];
-  } else {
-    return $class->$orig(@_);
-  }
-  $json = _get_from_thingi_given_id($id);
-  $hash = decode_json($json);
-  $hash->{_original_json} = $json;
-  return $hash;
-};
+__PACKAGE__->thingiverse_attributes(
+    {
+        api_base => '/copies/',
+        pk => { id => { isa => 'ID' } },
+        fields => {
+            url => { isa => 'Str' },
+            public_url => { isa => 'Str' },
+            added => { isa => 'Str' },
+            like_count => { isa => 'ThingiCount' },
+            description => { isa => 'Str' },
+            is_liked => { isa => 'Any' },
+            maker => { isa => 'Thingiverse::User' },
+            thumbnail => { isa => 'Str' },
+            images_url => { isa => 'Str' },
+        },
+    }
+);
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
